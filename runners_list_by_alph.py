6baf18071e06bd3713259517f1b07218ec9dc286
Денис Мурашов <murashov@zelrun.com>
2023-09-02 15:58:22 +0600

    first commit

---
+++ runners_list_by_alph.py
@@ -0,0 +1,131 @@
+import csv
+from fpdf import FPDF
+
+# Читаем CSV файл с данными клиентов
+# (csv-файл BIB;фамилия;имя;дата рождения в формате гггг-мм-дд)
+with open('list.csv', encoding='utf-8-sig', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    # Сортируем данные по фамилии
+    sorted_data = sorted(reader, key=lambda row: row[1])
+
+# Создаем PDF файл
+pdf = FPDF()
+pdf.add_page()
+
+# Устанавливаем шрифт и размер
+pdf.add_font('Ubuntu', '', 'UbuntuCondensed-Regular.ttf', uni=True)
+pdf.set_font('Ubuntu', size=10)
+
+# Параметры листа A4
+PAGE_WIDTH = 210
+PAGE_HEIGHT = 297
+COLUMN_WIDTH = PAGE_WIDTH / 2
+LAST_LETTER = '0'
+
+
+# Определяем высоту строки
+ROW_HEIGHT = pdf.font_size * 1.5
+
+# Определяем максимальное количество строк на странице
+MAX_ROWS = 100
+
+# Определяем текущие координаты курсора
+x = 10
+y = 10
+
+# Опреелем значение индекса колонок
+column_index = 0
+
+# Устанавливаем курсор на первую колонку
+first_column = True
+
+
+def check_new_letter(row) -> bool:
+    """Проверяем наличие новой буквы"""
+    global LAST_LETTER
+    if row[1][0] != LAST_LETTER:
+        print(f'{row[1][0]} - {row[1]}')
+        LAST_LETTER = row[1][0]
+        return True
+    return False
+
+
+def get_year(date: str) -> int:
+    """Достаем год из даты рождения"""
+    return date.split('-')[0]
+
+
+def column_switcher(value: bool) -> bool:
+    """Переключалка колонок"""
+    return True if value is False else False
+
+
+def add_row(row):
+    """Добавляем строку"""
+    if check_new_letter(row):
+        global x
+        x -= 5
+        pdf.set_x(x)
+        pdf.cell(
+            5,
+            5,
+            # Выводим новую букву
+            txt=f"{LAST_LETTER}", ln=0
+        )
+        # global column_index
+        # column_index += 1
+        x += 5
+        pdf.set_x(x)
+
+    pdf.cell(
+            COLUMN_WIDTH - 20,
+            ROW_HEIGHT,
+            # формат строки Фамилия Имя (год рождения) – BIB
+            txt=f"{row[1]} {row[2]} ({get_year(row[3])}) – {row[0]}", ln=1
+        )
+
+
+# Добавляем данные в PDF файл
+for i, row in enumerate(sorted_data):
+    column_index += 1
+    if column_index > 50:
+        column_index = 1
+        first_column = column_switcher(first_column)
+        y = 10
+
+    # Если достигнуто макс количество строк на странице, добавляем новую стр
+    if i % MAX_ROWS == 0 and i > 0:
+        pdf.add_page()
+        first_column = True
+        # Сбрасываем координаты на начало страницы
+        x = 10
+        y = 10
+
+    # Если это первая колонка, добавляем данные на текущую страницу
+    if first_column is True:
+        # Если достигнут конец страницы, переходим на следующую колонку
+        if y + ROW_HEIGHT > PAGE_HEIGHT:
+            x += COLUMN_WIDTH
+            y = 10
+
+        # Добавляем данные в первую колонку
+        add_row(row)
+        y += ROW_HEIGHT
+
+    # Если это вторая колонка, добавляем данные на следующую страницу
+    if first_column is False:
+        # Если достигнут конец страницы, переходим на новую страницу
+        if y + ROW_HEIGHT > PAGE_HEIGHT:
+            pdf.add_page()
+            first_column = True
+            # Сбрасываем координаты на начало страницы
+            x = 10
+            y = 10
+        x = 10 + COLUMN_WIDTH
+        # Добавляем данные во вторую колонку
+        pdf.set_xy(x, y)
+        add_row(row)
+        y += ROW_HEIGHT
+
+# Сохраняем PDF файл
+pdf.output("list_by_alphabet.pdf")
---
+++ runners_list_by_number.py
@@ -0,0 +1,114 @@
+import csv
+from fpdf import FPDF
+
+# Читаем CSV файл с данными клиентов
+with open('list.csv', encoding='utf-8-sig', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    # Сортируем данные по фамилии
+    sorted_data = sorted(reader, key=lambda row: int(row[0]))
+
+HUNDRED = int(sorted_data[0][0]) // 100
+
+# Создаем PDF файл
+pdf = FPDF()
+pdf.add_page()
+pdf.add_font('Ubuntu', '', 'UbuntuCondensed-Regular.ttf', uni=True)
+pdf.set_font('Ubuntu', size=10)
+
+# Параметры листа A4
+PAGE_WIDTH = 210
+PAGE_HEIGHT = 297
+COLUMN_WIDTH = PAGE_WIDTH / 2
+# Определяем высоту строки
+ROW_HEIGHT = pdf.font_size * 1.5
+
+# Определяем максимальное количество строк на странице
+MAX_ROWS = 100
+
+# Определяем текущие координаты курсора
+x = 10
+y = 10
+
+# Опреелем значение индекса колонок
+column_index = 0
+
+# Устанавливаем курсор на первую колонку
+first_column = True
+
+
+def column_switcher(value: bool) -> bool:
+    """Переключалка колонок"""
+    return True if value is False else False
+
+
+def get_year(date: str) -> int:
+    return date.split('-')[0]
+
+
+def add_row(row):
+    """Добавляем строку"""
+    pdf.cell(
+            COLUMN_WIDTH - 20,
+            ROW_HEIGHT,
+            txt=f"{row[0]}   {row[1]} {row[2]} ({get_year(row[3])})", ln=1
+        )
+
+
+# Добавляем данные в PDF файл
+for i, row in enumerate(sorted_data):
+    column_index += 1
+    if column_index > 50:
+        column_index = 1
+        first_column = column_switcher(first_column)
+        y = 10
+
+    # Если достигнуто макс количество строк на странице, добавляем новую стр
+    if int(row[0]) // 100 != HUNDRED:
+        HUNDRED = int(row[0]) // 100
+        pdf.add_page()
+        first_column = True
+        column_index = 1
+        # Сбрасываем координаты на начало страницы
+        x = 10
+        y = 10
+
+    # Если это первая колонка, добавляем данные на текущую страницу
+    if first_column is True:
+        # Если достигнут конец страницы, переходим на следующую колонку
+        if y + ROW_HEIGHT > PAGE_HEIGHT:
+            x += COLUMN_WIDTH
+            y = 10
+
+        # Добавляем данные в первую колонку
+        add_row(row)
+        pdf.line(
+            x1=x-3,
+            y1=y+ROW_HEIGHT,
+            x2=x+COLUMN_WIDTH-40,
+            y2=y+ROW_HEIGHT
+        )
+        y += ROW_HEIGHT
+
+    # Если это вторая колонка, добавляем данные на следующую страницу
+    if first_column is False:
+        # Если достигнут конец страницы, переходим на новую страницу
+        if y + ROW_HEIGHT > PAGE_HEIGHT:
+            pdf.add_page()
+            first_column = True
+            # Сбрасываем координаты на начало страницы
+            x = 10
+            y = 10
+        x = 10 + COLUMN_WIDTH
+        # Добавляем данные во вторую колонку
+        pdf.set_xy(x, y)
+        add_row(row)
+        pdf.line(
+            x1=x-3,
+            y1=y+ROW_HEIGHT,
+            x2=x+COLUMN_WIDTH-40,
+            y2=y+ROW_HEIGHT
+        )
+        y += ROW_HEIGHT
+
+# Сохраняем PDF файл
+pdf.output("list_by_numbers.pdf")
